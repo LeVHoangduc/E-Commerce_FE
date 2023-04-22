f56991a15f4fbc2f700f45959e8b1405dff98951
LeVHoangduc <levhoangduc@gmail.com>
2023-04-22 12:59:49 +0700

    donepagi

--- src/pages/ProductList/Product/Product.tsx
+++ src/pages/ProductList/Product/Product.tsx
@@ -1,35 +1,37 @@
 import { Link } from 'react-router-dom'
-
-export default function Product() {
+import ProductRating from 'src/components/ProductRating'
+import { Product as ProductType } from 'src/types/product.type'
+import { formatCurrency, formatNumberToSocialStyle } from 'src/utils/utils'
+interface Props {
+  product: ProductType
+}
+export default function Product({ product }: Props) {
   return (
     <Link to='/'>
       <div className='overflow-hidden rounded-sm bg-white shadow transition-transform duration-100 hover:translate-y-[-0.04rem] hover:shadow-md'>
         <div className='relative w-full pt-[100%] '>
-          <img src='' alt='' className='absolute top-0 left-0 h-full w-full bg-white object-cover' />
+          <img
+            src={product.image}
+            alt={product.name}
+            className='absolute top-0 left-0 h-full w-full bg-white object-cover'
+          />
         </div>
         <div className='overflow-hidden p-2'>
-          <div className='min-h-[2rem] text-xs line-clamp-2'>dadsa</div>
+          <div className='min-h-[2rem] text-xs line-clamp-2'>{product.name}</div>
           <div className='mt-3 flex items-center'>
             <div className='max-w-[50%] truncate text-gray-500 line-through'>
               <span>d</span>
-              <span>5.000</span>
+              <span>{formatCurrency(product.price_before_discount)}</span>
             </div>
             <div className='ml-1 truncate text-orange'>
               <span className='text-xs'>d</span>
-              <span>2.000</span>
+              <span>{formatCurrency(product.price)}</span>
             </div>
           </div>
           <div className='mt-3 flex items-center justify-end'>
-            <div className='flex items-center'>
-              <div className='relative'>
-                <div className='absolute top-0 left-0 h-full overflow-hidden' style={{ width: '100%' }}>
-                  <svg></svg>
-                </div>
-                <svg></svg>
-              </div>
-            </div>
+            <ProductRating rating={product.rating} />
             <div className='ml-2 text-sm'>
-              <span>5.66k</span>
+              <span>{formatNumberToSocialStyle(product.sold)}</span>
               <span className='ml-1'>Sold</span>
             </div>
           </div>
